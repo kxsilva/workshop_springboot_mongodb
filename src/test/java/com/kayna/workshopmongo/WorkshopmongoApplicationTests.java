c0aa5453bb6171b94ea3a0fc9a28b5bfff7e8fc2
Kaynã Xavier <kayna.xavier@ufpe.br>
2023-02-13 18:56:14 -0300

    projeto workshop criado

---
+++ src/test/java/com/kayna/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayna.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
