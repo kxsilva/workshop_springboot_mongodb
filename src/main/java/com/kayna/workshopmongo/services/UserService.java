4d25f3b6caef2c5845598f2c328cbdb68caa7912
Kaynã Xavier <kayna.xavier@ufpe.br>
2023-02-15 20:01:10 -0300

    Inserção de usuário com Post

--- src/main/java/com/kayna/workshopmongo/services/UserService.java
+++ src/main/java/com/kayna/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kayna.workshopmongo.domain.User;
+import com.kayna.workshopmongo.dto.UserDTO;
 import com.kayna.workshopmongo.repository.UserRepository;
 import com.kayna.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
