0052c287a6de8e8440e4d176d0a49e50ac0a59f4
Kaynã Xavier <kayna.xavier@ufpe.br>
2023-02-15 20:47:12 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/kayna/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.kayna.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kayna.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
