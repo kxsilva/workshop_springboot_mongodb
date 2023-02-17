9f6c39e01bbd41addb6b27c93c4859e36ed154b3
Kaynã Xavier <kayna.xavier@ufpe.br>
2023-02-17 19:10:38 -0300

    Obtendo Post por Id

---
+++ src/main/java/com/kayna/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.kayna.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kayna.workshopmongo.domain.Post;
+import com.kayna.workshopmongo.repository.PostRepository;
+import com.kayna.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
